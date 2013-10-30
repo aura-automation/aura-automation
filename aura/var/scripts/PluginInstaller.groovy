dc1917d41d5b6c9fc53251d04f2e011e89718ac9
Jatin Bhadra <jatin@Jatins-MacBook-Pro.local>
2013-10-30 15:33:18 +0000

    individual check of plugin files

--- aura/var/scripts/PluginInstaller.groovy
+++ aura/var/scripts/PluginInstaller.groovy
@@ -15,10 +15,7 @@ public class PluginInstaller{
 	def propsDir 
 	
 	void process(){
-		boolean pluginInstalled = checkWASPlugin()
-		if (!pluginInstalled ){
-			installPlugin()
-		}
+		installPlugin()
 	}
 
 	void installPlugin(){
@@ -42,58 +39,87 @@ public class PluginInstaller{
 		destination.lastModified = source.lastModified()
 	}
 	
+	boolean shouldPrompt(String fileDir,String fileName){
+		println "checking $fileDir file $fileName"
+		def shouldPrompt = true	
+		if ((new File(fileDir)).exists()){
+			((new File(fileDir))).eachFile{
+				if (it.name =~ fileName)
+					shouldPrompt = false
+			}
+		}
+		shouldPrompt
+	}
+	
 	void promptAdminClientJar(){
-		Validator validator = new Validator()
-		validator.addFileValidation()
-		validator.hasString("com.ibm.ws.admin.client")
-		
-		def adminClientJar = common.prompt(null, "Enter the location of com.ibm.ws.admin.client jar e.g. c:/WebSphere/AppServer/runtimes/com.ibm.ws.admin.client_8.0.0.jar","",validator)
-		
-		copy (adminClientJar , libDir)
+		def fileName = "com.ibm.ws.admin.client"
+		if (shouldPrompt(libDir,fileName)){	
+			Validator validator = new Validator()
+			validator.addFileValidation(fileName)
+			validator.hasString()
+			
+			def adminClientJar = common.prompt(null, "Enter the location of com.ibm.ws.admin.client jar e.g. c:/WebSphere/AppServer/runtimes/com.ibm.ws.admin.client_8.0.0.jar","",validator)
+			
+			copy (adminClientJar , libDir)
+		}
 	}
 	
 	void promptCryptoJar(){
-		Validator validator = new Validator()
-		validator.addFileValidation()
-		validator.hasString("com.ibm.ws.security.crypto")
-		
-		def cryptoJar = common.prompt(null, "Enter the location of com.ibm.ws.security.crypto jar e.g. c:/WebSphere/AppServer/runtimes/security.crypto.jar","",validator)
-		
-		copy (cryptoJar , libDir)
+		def fileName = "com.ibm.ws.security.crypto"
+		if (shouldPrompt(libDir,fileName)){	
 
+			Validator validator = new Validator()
+			validator.addFileValidation(fileName)
+			validator.hasString()
+			
+			def cryptoJar = common.prompt(null, "Enter the location of com.ibm.ws.security.crypto jar e.g. c:/WebSphere/AppServer/runtimes/security.crypto.jar","",validator)
+			
+			copy (cryptoJar , libDir)
+		}
 	}
 	
 	void promptSecProperties(){
 		Validator validator = new Validator()
 		validator.addPropsFileValidation()
 
-		def secPropsDir = common.prompt(null, "Enter the location of dir containing sas, soap client props e.g. c:/WebSphere/AppServer/properties","",validator)
+		def fileName = "com.ibm.ws.security.crypto"
+		if ( shouldPrompt(propsDir,"sas.client.props") 
+			|| shouldPrompt(propsDir, "soap.client.props") 
+			|| shouldPrompt(propsDir, "ssl.client.props")
+			){	
 		
-		copy (secPropsDir + File.separator + "sas.client.props" , propsDir) 
-		copy (secPropsDir + File.separator + "soap.client.props" , propsDir) 
-		copy (secPropsDir + File.separator + "ssl.client.props" , propsDir)
-		// parse the ssl config file and change user root to plugins dir. This is to enable loading of certs
-		ConfigurationManager confManager = new ConfigurationManager()
-		confManager.setProperty(propsDir + File.separator + "ssl.client.props",
-			,"user.root"
-			,plugindir)
+			def secPropsDir = common.prompt(null, "Enter the location of dir containing sas, soap client props e.g. c:/WebSphere/AppServer/properties","",validator)
+			
+			copy (secPropsDir + File.separator + "sas.client.props" , propsDir) 
+			copy (secPropsDir + File.separator + "soap.client.props" , propsDir) 
+			copy (secPropsDir + File.separator + "ssl.client.props" , propsDir)
+			// parse the ssl config file and change user root to plugins dir. This is to enable loading of certs
+			ConfigurationManager confManager = new ConfigurationManager()
+			confManager.setProperty(propsDir + File.separator + "ssl.client.props",
+				,"user.root"
+				,plugindir)
+			}
 
 	}
 	
 	void promptSSLStore(){
-		Validator validator = new Validator()
-		validator.addFileValidation()
-
-		def keyStoreFile = common.prompt(null, "Enter the location of key store file,  e.g. c:/WebSphere/AppServer/etc/key.p12","",validator)	
-		def trustStoreFile = common.prompt(null, "Enter the location of trust store file e.g. c:/WebSphere/AppServer/etc/trust.p12","",validator)
-		
-		copy (keyStoreFile, etcDir)
-		copy (trustStoreFile, etcDir)
+	if ( shouldPrompt(etcDir,"key") 
+			|| shouldPrompt(etcDir, "trust") 
+			){	
+			Validator validator = new Validator()
+			validator.addFileValidation()
+	
+			def keyStoreFile = common.prompt(null, "Enter the location of key store file,  e.g. c:/WebSphere/AppServer/etc/key.p12","",validator)	
+			def trustStoreFile = common.prompt(null, "Enter the location of trust store file e.g. c:/WebSphere/AppServer/etc/trust.p12","",validator)
+			
+			copy (keyStoreFile, etcDir)
+			copy (trustStoreFile, etcDir)
+		}
 			
 	}
 	
 	boolean checkWASPlugin(){
-		println("Check if plugin installed in location " + auraRepo)
+		println("Check if plugin installed at the location " + auraRepo)
 		boolean pluginInstalled = true
 		if (!(new File(etcDir)).exists()){
 			pluginInstalled = false
